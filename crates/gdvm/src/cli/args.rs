09ad80e142a48e6a09a0559b78c857eebf5198cb
Adaline Simonian <adalinesimonian@pm.me>
2026-07-18 02:08:40 +0200

    chore: formatting fix

--- crates/gdvm/src/cli/args.rs
+++ crates/gdvm/src/cli/args.rs
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
 
-use clap::{ArgAction, Arg, Command, value_parser};
+use clap::{Arg, ArgAction, Command, value_parser};
 use gdvm::config::{self};
 use gdvm::t;
 use gdvm::version::{self};
